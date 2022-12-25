a1ab83e588893d861e65113d17feb4a5650c2d04
tomatozoo <enkeejuniour@naver.com>
2022-12-25 22:38:24 +0900

    feat: Fix settings.py for social login

--- dear_j/dear_j/config.py
+++ dear_j/dear_j/config.py
@@ -17,3 +17,19 @@ JWT_AUTH_COOKIE = "my-app-auth"
 JWT_AUTH_REFRESH_COOKIE = "my-refresh-token"
 ACCESS_TOKEN_LIFETIME = datetime.timedelta(hours=2)
 REFRESH_TOKEN_LIFETIME = datetime.timedelta(days=7)
+
+SOCIALACCOUNT_PROVIDERS = {
+    'kakao': {
+        'APP': {
+            'client_id': "c3cd86b188ba4bced22763199ea4f374",
+            'secret': 842576,
+            'key': ''
+        }
+    }
+}
+
+
+ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
+LOGIN_REDIRECT_URL = "/"
+ACCOUNT_AUTHENTICATED_LOGOUT_REDIRECTS = True
+ACCOUNT_LOGOUT_REDIRECT_URL = "/"
